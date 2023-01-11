1094c7cf0a7c0b7669fd224e255058f5cdfe9bfb
Sangeethaposam <120437076+Sangeethaposam@users.noreply.github.com>
2023-01-11 15:37:44 +0530

    Add files via upload

---
+++ EmployeeWageMain.java
@@ -0,0 +1,10 @@
+package com.bridzelabz.employeeWage;
+
+public class EmployeeWageMain {
+
+	public static void main(String[] args) {
+		EmployeeWage emp =new EmployeeWage();
+        emp.wagesTillaCondition();
+	}
+
+}
